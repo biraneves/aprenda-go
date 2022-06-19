5eef434db74fb6ce6d242c0baee5a17fed7a7233
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-19 09:35:02 -0300

    Sistemas numéricos.

---
+++ cap04 - fundamentos-de-programacao/cap04-04.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	a := 100
+	b := 1000
+	c := 31337
+
+	fmt.Printf("%d\t%b\t%#x\n", a, a, a)
+	fmt.Printf("%d\t%b\t%#x\n", b, b, b)
+	fmt.Printf("%d\t%b\t%#x\n", c, c, c)
+
+}
