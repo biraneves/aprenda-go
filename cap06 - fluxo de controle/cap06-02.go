3c5a12fe6cd1661e8a59101e7624fc9967d9f6ea
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-19 11:10:18 -0300

    Loops aninhados

---
+++ cap06 - fluxo de controle/cap06-02.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	for horas := 0; horas < 12; horas++ {
+
+		fmt.Println("Hora:", horas)
+
+		for minutos := 0; minutos < 60; minutos++ {
+			
+			fmt.Print(" ", minutos)
+
+		}
+
+		fmt.Println("")
+
+	}
+
+}
