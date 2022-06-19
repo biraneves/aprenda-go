5e79d457b34770fa8935cc0b03e2df383cc90580
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-19 10:33:39 -0300

    Nível 2 - exercício 2

---
+++ cap05 - exercicios-02/cap05-02.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+const a int = 20
+const b int = 30
+
+func main() {
+
+	c := a == b
+	d := a <= b
+	e := a > b
+	f := a < b
+
+	fmt.Printf("a = %v\n", a)
+	fmt.Printf("b = %v\n", b)
+	linha("=")
+	fmt.Printf("a == b: %v\n", c)
+	fmt.Printf("a <= b: %v\n", d)
+	fmt.Printf("a > b: %v\n", e)
+	fmt.Printf("a < b: %v\n", f)
+
+}
+
+func linha(char string) {
+	linha := ""
+	for i := 0; i < 64; i++ {
+		linha += char
+	}
+	fmt.Println(linha)
+}
