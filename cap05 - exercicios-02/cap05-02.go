63f1e5a07f012fe24a62ebf42c059c418366ac7c
Ubirajara Neves <ubirajara.neves@usp.br>
2022-06-19 10:41:40 -0300

    Acrescentados alguns operadores relacionais

--- cap05 - exercicios-02/cap05-02.go
+++ cap05 - exercicios-02/cap05-02.go
@@ -9,16 +9,20 @@ func main() {
 
 	c := a == b
 	d := a <= b
-	e := a > b
-	f := a < b
+	e := a < b
+	f := a >= b
+	g := a > b
+	h := a != b
 
 	fmt.Printf("a = %v\n", a)
 	fmt.Printf("b = %v\n", b)
 	linha("=")
 	fmt.Printf("a == b: %v\n", c)
 	fmt.Printf("a <= b: %v\n", d)
-	fmt.Printf("a > b: %v\n", e)
-	fmt.Printf("a < b: %v\n", f)
+	fmt.Printf("a < b: %v\n", e)
+	fmt.Printf("a >= b: %v\n", f)
+	fmt.Printf("a > b: %v\n", g)
+	fmt.Printf("a != b: %v\n", h)
 
 }
 
